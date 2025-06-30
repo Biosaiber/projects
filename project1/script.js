548edf625a6c01c2b2e696d78a06faa003d80b3e
isubfox <samuel.liska@gmail.com>
2025-06-30 13:29:45 +0200

    event listener and checkbox for done task

--- project1/script.js
+++ project1/script.js
@@ -3,9 +3,9 @@ console.log("script loaded");
 // empty template for factory function:
 function createTodo(text) {
   return {
-    id: Date.now(),       // unique ID pomocou timestampu
-    text: text,           // samotný text úlohy
-    completed: false      // či je dokončená
+    id: Date.now(), // unique ID pomocou timestampu
+    text: text, // samotný text úlohy
+    completed: false, // či je dokončená
   };
 }
 
@@ -15,16 +15,50 @@ function renderTodos(todos) {
   const list = document.getElementById("todo-list");
   list.innerHTML = ""; // clear list
 
-  todos.forEach(todo =>{
+  todos.forEach((todo) => {
     const li = document.createElement("li");
-    li.textContent = todo.text;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => {
+      todo.completed = checkbox.checked;
+      renderTodos(taskList);
+    });
+
+    const p = document.createElement("p");
+    p.textContent = todo.text;
+    if (todo.completed) {
+      p.style.textDecoration = "line-through";
+    }
+    
+    li.appendChild(checkbox);
+    li.appendChild(p);
     list.appendChild(li);
+
+
   });
 }
 
 const newTask = createTodo("Urobiť domácu úlohu");
-
 const newTask2 = createTodo("halooo");
 
+taskList.push(newTask, newTask2);
 
 renderTodos(taskList);
+
+const input = document.getElementById("todo-input");
+const addButton = document.getElementById("add-task");
+
+addButton.addEventListener("click", () => {
+  const text = input.value.trim();
+  if (text === "") {
+    alert("Please enter task description!");
+    return;
+  }
+
+  const newTask = createTodo(text);
+  taskList.push(newTask);
+  renderTodos(taskList);
+  input.value = "";
+});
